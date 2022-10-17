8ff7079f8d8fc42ab0d898d84b70bfb6b9e06b21
datnt2711 <datnt271196@gmail.com>
2022-10-17 18:15:11 +0700

    fix-version

---
+++ src/views/Demo/MyComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return <div>Welcome to Class Component with Dat ei</div>;
+  }
+}
+
+export default MyComponent;
